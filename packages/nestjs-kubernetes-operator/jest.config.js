568738493fb4dea8426c82d2244cc9fa80c05066
julius.biskopsto <julius.biskopsto@globetracker.com>
2021-09-03 13:32:04 +0100

    ci: Added circleci

---
+++ packages/nestjs-kubernetes-operator/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: "nestjs-kubernetes-operator",
+  preset: "../../jest.preset.js",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+    },
+  },
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[tj]sx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/packages/nestjs-kubernetes-operator",
+};
