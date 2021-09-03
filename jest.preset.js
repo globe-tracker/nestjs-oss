182ba0bf70a7fc2dc056af4b4ca4419bd2f72e17
julius.biskopsto <julius.biskopsto@globetracker.com>
2021-09-03 13:59:02 +0100

    refactor: changed to yarn, enabled nx cloud

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,20 @@
 const nxPreset = require("@nrwl/jest/preset");
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        suiteName: "jest tests",
+        outputDirectory: "test-results",
+        uniqueOutputName: "true",
+        classNameTemplate: "{classname}-{title}",
+        titleTemplate: "{classname}-{title}",
+        ancestorSeparator: " › ",
+        usePathForSuiteName: "true",
+      },
+    ],
+  ],
+};
