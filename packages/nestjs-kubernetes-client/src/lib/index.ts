c101c721bbc768ea9031f6fb827db2fb882e2476
julius.biskopsto <julius.biskopsto@globetracker.com>
2021-09-06 09:50:36 +0100

    feat(kubernetes-client): exported configuration objects

--- packages/nestjs-kubernetes-client/src/lib/index.ts
+++ packages/nestjs-kubernetes-client/src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from "./interfaces";
+export * from "./config/configurations";
 export * from "./constants";
 export * from "./kubernetes-client.module";
 export * from "./kubernetes-client.service";
